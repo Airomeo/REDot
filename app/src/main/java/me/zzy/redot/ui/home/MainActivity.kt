ad9337749c2d6739d5598d8ad2b5fcfe6f9753eb
ZZY <airomeoe@gmail.com>
2021-01-06 19:48:06 +0800

    标题栏会随着视图的滚动：显示和隐藏 添加按钮点击跳转设置功能

--- app/src/main/java/me/zzy/redot/ui/home/MainActivity.kt
+++ app/src/main/java/me/zzy/redot/ui/home/MainActivity.kt
@@ -1,44 +1,42 @@
 package me.zzy.redot.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 import me.zzy.redot.R
+import me.zzy.redot.ui.design.TextSettingActivity
 
 class MainActivity : AppCompatActivity() {
-    private var recyclerView: RecyclerView? = null
     private var mAdapter: RecyclerView.Adapter<*>? = null
     private var layoutManager: RecyclerView.LayoutManager? = null
     private val myDataset = arrayOf("aaa", "bbb", "ccc", "ddd")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-        recyclerView = findViewById<View>(R.id.list) as RecyclerView
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
-        recyclerView!!.setHasFixedSize(true)
+        list!!.setHasFixedSize(true)
 
         // use a linear layout manager
         layoutManager = LinearLayoutManager(this)
-        recyclerView!!.layoutManager = layoutManager
+        list!!.layoutManager = layoutManager
 
         // specify an adapter (see also next example)
         mAdapter = MyAdapter(myDataset)
-        recyclerView!!.adapter = mAdapter
-        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        list!!.adapter = mAdapter
         fab.setOnClickListener { view: View? ->
-            Snackbar.make(view!!, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            startActivity(Intent(this, TextSettingActivity::class.java))
+
+//            Snackbar.make(view!!, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
         }
     }
 
